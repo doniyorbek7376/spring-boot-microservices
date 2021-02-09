ef68602a733ca09554a0aecf37fa2d0df6b7f328
doniyorbek7376 <doniyorbek6439@gmail.com>
2021-02-09 11:38:57 +0500

    add default constructor

--- movie-catalog-service/src/main/java/uz/doniyorbek7376/moviecatalogservice/models/Movie.java
+++ movie-catalog-service/src/main/java/uz/doniyorbek7376/moviecatalogservice/models/Movie.java
@@ -4,6 +4,9 @@ public class Movie {
     private String movieId;
     private String name;
 
+    public Movie() {
+    }
+
     public Movie(String movieId, String name) {
         this.setMovieId(movieId);
         this.setName(name);
