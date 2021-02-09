2ac116704b90af2e8baf7ac44eaabf47eeafbd03
doniyorbek7376 <doniyorbek6439@gmail.com>
2021-02-09 14:34:10 +0500

    added eureka server

--- ratings-data-service/src/main/java/uz/doniyorbek7376/ratingsdataservice/resources/RatingsDataResources.java
+++ ratings-data-service/src/main/java/uz/doniyorbek7376/ratingsdataservice/resources/RatingsDataResources.java
@@ -1,7 +1,6 @@
 package uz.doniyorbek7376.ratingsdataservice.resources;
 
 import java.util.Arrays;
-import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
