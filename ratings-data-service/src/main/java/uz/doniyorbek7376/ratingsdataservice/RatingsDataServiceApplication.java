2545ba64847842aab5cd252640da48189f5dfb9f
doniyorbek7376 <doniyorbek6439@gmail.com>
2021-02-09 10:33:12 +0500

    Initial commit

---
+++ ratings-data-service/src/main/java/uz/doniyorbek7376/ratingsdataservice/RatingsDataServiceApplication.java
@@ -0,0 +1,13 @@
+package uz.doniyorbek7376.ratingsdataservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatingsDataServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatingsDataServiceApplication.class, args);
+	}
+
+}
